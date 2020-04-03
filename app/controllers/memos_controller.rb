5c8292b384b0caa23aaab16ab52679bd1c4e52d5
tosiran <f2.ree_eat_10470@icloud.com>
2020-04-03 13:42:09 +0900

    メモ編集　削除機能完了

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -12,6 +12,18 @@ class MemosController < ApplicationController
     @memo = Memo.find(params[:id])
   end
 
+  def update
+    memo = Memo.find(params[:id])
+    memo.update(memo_params)
+    redirect_to memo_path(memo.id)
+  end
+
+  def destroy
+    memo = Memo.find(params[:id])
+    memo.destroy
+    redirect_to root_path
+  end
+
   def create
     Memo.create(memo_params)
   end
